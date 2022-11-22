f8764ab6835c72a0e3b264b5201e3185066ef67e
Luis Silva <luansimo@outlook.com>
2022-11-22 14:14:10 -0600

    gtk3 boilerplate code funcional y comando de compilacion correcto

--- src/main.c
+++ src/main.c
@@ -12,9 +12,9 @@ int main (int argc, char* argv[]){
     GtkApplication *app;
     int status;
 
-    app = gtk_applicacion_new("edu.ibero.basesproyecto", G_APPLICATION_FLAGS_NONE);
+    app = gtk_application_new("edu.ibero.basesproyecto", G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-    status = g_application_run(G_APPLIACTION(app), argc, argv);
+    status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
 
     return status;
