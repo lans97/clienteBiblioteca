97b9c03804492fd401d95c4db3e6c744126808f4
Luis Silva <a200490a@correo.uia.mx>
2022-11-24 09:35:39 -0600

    script creates.sql funcional

--- sqlscripts/creates.sql
+++ sqlscripts/creates.sql
@@ -8,20 +8,20 @@ CREATE TABLE py_users(
 	f_nac DATE,
 	email VARCHAR(30),
 	passwd VARCHAR(20),
-	admin BOOLEAN,
-)
+	admin BOOLEAN
+);
 
 CREATE TABLE py_libros(
 	isbn INT PRIMARY KEY,
 	nombre VARCHAR(50),
 	editorial VARCHAR(20),
 	prestados INT,
-	disponibles INT,
-)
+	disponibles INT
+);
 
 CREATE TABLE py_solicitudes(
 	id INT PRIMARY KEY,
 	isbn INT REFERENCES py_libros(isbn),
 	n_cuenta INT REFERENCES py_users(n_cuenta),
-	f_devolucion DATE,
-)
+	f_devolucion DATE
+);
